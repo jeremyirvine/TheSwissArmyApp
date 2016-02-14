ca31a29d3343c1dad3a1476db357382dc6c2fdc6
jezza23 <JezzaGaming@Jeremys-MacBook.local>
2016-02-13 22:47:49 -0500

    2nd commit

---
+++ TheSwissArmyApp/TheSwissArmyApp/CalculatorViewController.swift
@@ -0,0 +1,22 @@
+//
+//  InstagramViewController.swift
+//  Project Feed
+//
+//  Created by Timothy Gurguis on 2/13/16.
+//  Copyright © 2016 Timothy Gurguis. All rights reserved.
+//
+
+import UIKit
+
+class CalculatorViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
